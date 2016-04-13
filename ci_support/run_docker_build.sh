c90a8c9c9ebf898086a61aa888f635df161a9545
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-13 13:57:16 -0400

    yum: Add OpenGL to the requirements.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -43,9 +43,6 @@ conda update --yes --all
 conda install --yes conda-build==1.18.2
 conda info
 
-# Temporary fix to install opengl drivers
-yum install -y mesa-libGL-devel
-
 # A better way to handle yum requirements.
 test -f "recipe_root/yum_requirements.txt" && (cat "recipe_root/yum_requirements.txt" | xargs -r yum install -y) || true
 
